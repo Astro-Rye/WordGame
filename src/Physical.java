2196cadc6e1b822de20c595b9701689ccf64bdae
Joseph Carrillo <98797753+JaayCarrillo@users.noreply.github.com>
2025-11-09 00:31:39 -0700

    updated physical to show an image when a prize is won

--- src/Physical.java
+++ src/Physical.java
@@ -1,7 +1,8 @@
+import javax.swing.*;
 import java.util.Random;
 
 public class Physical implements Award{
-        private static String [] prizes = {
+        private static String [] PRIZES = {
                 "Vacation",
                 "Car",
                 "Refrigerator",
@@ -17,20 +18,44 @@ public class Physical implements Award{
            "WordGame/resources/disney.jpeg",
            "WordGame/resources/cruise.jpeg"
         };
-    private final Random num = new Random();
-    int getRandomPrize(){
-         return num.nextInt(prizes.length);
+    private final Random rand = new Random();
+
+    int getRandomPrizeIndex(){
+         return rand.nextInt(PRIZES.length);
     }
     @Override
     public int displayWinnings(Players players, boolean correct) {
-        String name = (players.getFirstName() + " " + players.getLastName().trim());
-        String prize = prizes[getRandomPrize()];
+       int index = getRandomPrizeIndex();
+       String prizeName = PRIZES[index];
 
-        if(correct) {
-            System.out.println(name + ", you won a " + prize + "!");
-        } else {
-            System.out.println(name + ", you lost. You could have won a " + prize + ".");
+        ImageIcon icon = null;
+        String imagePath = PRIZE_IMAGES[index];
+
+        try {
+            icon = new ImageIcon(imagePath);
+        } catch ( Exception e ) {
+            // if image fails to load, skip the icon
         }
+        if(correct) {
+            String msg = players.getFirstName() + "won: " + prizeName + "!";
+            JOptionPane.showMessageDialog(
+                    null,
+                    msg,
+                    "Physical Prize Won!",
+                    JOptionPane.INFORMATION_MESSAGE,
+                    icon
+            );
+        } else {
+            String msg = players.getFirstName() + ", you lost this round.\nYou *could* have won " + prizeName + ".";
+            JOptionPane.showMessageDialog(
+                    null,
+                    msg,
+                    "So Close!",
+                    JOptionPane.INFORMATION_MESSAGE,
+                    icon
+            );
+        }
+        // Physical prizes doont change money for now ....  [come back after]
         return 0;
     }
 }
